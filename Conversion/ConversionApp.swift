7ccf7812a15c80a07d38b8baf1b2f388e3181830
Arjun <arjun@arjuns-mbp.lan>
2021-12-30 17:50:41 -0500

    add to git

---
+++ Conversion/ConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionApp.swift
+//  Conversion
+//
+//  Created by Arjun on 2021-04-23.
+//
+
+import SwiftUI
+
+@main
+struct ConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
